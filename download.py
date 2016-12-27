6648942b576f0c4675556d96e57b67c312ed6136
Brandon Roberts <brandon@bxroberts.org>
2016-12-26 17:24:53 -0800

    save download scripts

---
+++ download.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python2.7
+"""Script for downloading Seinfeld scripts."""
+
+from __future__ import division
+import argparse
+import os
+import sys
+import time
+
+import requests
+
+
+
+def _format_num(n):
+    return '%02d' % n
+
+EPISODE_NUMBERS = (
+    map(_format_num, range(1, 82)) +
+
+    # Double episode
+    ['82and83'] +
+
+    map(_format_num, range(84, 100)) +
+
+    # Skip the clip show "100and101".
+
+    map(_format_num, range(102, 177)) +
+
+    # Skip the clip show "177and178".
+
+    # Double episode (Finale)
+    ['179and180']
+)
+
+URL = 'http://www.seinology.com/scripts/script-%s.shtml'
+
+
+def get_script_html(episode_number):
+    url = URL % episode_number
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    return resp.text
+
+
+def main(args):
+    episode_numbers = (
+        args.episode_number if args.episode_number is not None
+        else EPISODE_NUMBERS
+    )
+
+    num_episodes = len(episode_numbers)
+    for idx, episode_number in enumerate(episode_numbers, start=1):
+        print >> sys.stderr, '[Ep. %s]\t\t%d of %d (%.2f%%)' %  (
+            episode_number, idx,
+            len(episode_numbers),
+            idx / num_episodes * 100
+        )
+
+        out_path = os.path.join(
+            args.output_directory,
+            '%s.shtml' % episode_number
+        )
+
+
+        if args.no_overwrite is True and os.path.exists(out_path):
+            continue
+
+        script_html = get_script_html(episode_number)
+
+        with open(out_path, 'w') as fh:
+            print >> fh, script_html
+
+        if idx != num_episodes:
+            time.sleep(args.sleep_seconds)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        'output_directory',
+        help='The directory to write script HTML files to.'
+    )
+
+    parser.add_argument(
+        '--sleep-seconds',
+        type=float,
+        default=1.0,
+        help=('The number of seconds to sleep between downloading each page. '
+              'Defaults to 1 second.')
+    )
+
+    parser.add_argument(
+        '--no-overwrite',
+        action='store_false',
+        default=True,
+        help='If True then will not overwrite existing downloaded files.'
+    )
+
+    parser.add_argument(
+        '--episode-number',
+        action='append',
+        required=False,
+        choices=EPISODE_NUMBERS,
+        help=('An episode name or number (e.g. "79" or "82and83"). If '
+              'specified then only this episode will be downloaded '
+              'rather than all of them (the default). This can be specified '
+              'multiple times.')
+    )
+
+    main(parser.parse_args(sys.argv[1:]))
---
+++ scrape.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python2.7
+
+
+import re
+import sys
+
+
+
+def unescape(s):
+    """Replace HTML jibberish with normal symbols."""
+    s = s.replace("&lt;", "<")
+    s = s.replace("&gt;", ">")
+    s = s.replace("&#145;", "'")
+    s = s.replace("&#146;", "'")
+    s = s.replace("&#147;", "'")
+    s = s.replace("&#148;", "'")
+    s = s.replace("&#149;", "'")
+    s = s.replace("&#150;", "'")
+
+    s = s.replace("&quot;", "'")
+    s = s.replace("&#133;", "...")
+
+
+    # this has to be last:
+    s = s.replace("&amp;", "&")
+    return s
+
+
+def remove_tags(text):
+    """Returns the text with HTML tags removed."""
+    return re.sub(r'<.*?>', '', text)
+
+
+def parse_episode_info(html):
+    """Return a dict with meta-info about the episode."""
+    groups = re.search(r'pc: .*? season (\d+), episode (\d+)', html).groups()
+    season_num = int(groups[0])
+    episode_num = int(groups[1])
+
+    title = re.search(r'Episode \d+(.*?) - (.*?)<', html).groups()[1]
+    date = re.search(r'Broadcast date: (.*?)<', html).groups()[0]
+    writers = re.search(r'Written [bB]y:? (.*?)<', html).groups()[0]
+    writers = tuple([w.strip() for w in re.split(r',|&amp;', writers) if w])
+    director = re.search(r'Directed [bB]y (.*?)<', html).groups()[0]
+
+    return {'season_num': season_num, 'episode_num': episode_num, 
+            'title': title, 'date': date, 'writers': writers, 
+            'director': director}
+
+
+def parse_script(html):
+    """Returns a sequence of (speaker, utterance) pairs."""
+    utterances = re.findall(r'([A-Z]+)(?: \(.*?\))?: (.*?)</?(?:br|p)>', html)
+
+    for i, (speaker, utterance_text) in enumerate(utterances):
+        # Skip the monologues at the beginning of episodes.
+        if speaker.upper() == 'JERRY' and \
+              i == 0 and \
+              len(utterance_text.split()) > 100:
+            continue
+
+        yield (speaker, unescape(utterance_text))
+
+
+
+def scrape_episode(html):
+    html = html.replace('&nbsp;', ' ')
+    splitted = re.split(r'={30}.*', html)
+    info_html = splitted[0]
+    script_html = splitted[1]
+    info = parse_episode_info(info_html)
+
+    utterances = parse_script(script_html)
+    return (info, utterances)
+
+strip = r"\.\.\.|\.\.|\s'|'\s|\"|\(.*[\)\'\:]|\n|\r"
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print "USAGE: scrape.py script.shtml"
+        sys.exit(1)
+    with open(sys.argv[1]) as file:
+        info, utterances = scrape_episode(file.read())
+        last_line = None
+        for utterance in utterances:
+            speaker = utterance[0]
+            line = re.sub( "\s{2,}", " ", re.sub( strip, " ", utterance[1])).lower()
+            if last_line is None:
+                last_line = line
+                continue
+            if speaker.lower() == 'jerry' \
+                    and len(line) < 150 \
+                    and len(last_line) < 150:
+                print("{0}<Q>{1}<A>".format(
+                    last_line, line
+                ))
+            last_line = line
+
